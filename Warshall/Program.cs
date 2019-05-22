1d6ab057303012bdcc2c4e09874365798f443b80
Jamie Ballin <jamie.ballin@riskfirst.com>
2019-05-22 07:52:22 +0100

    Add project files.

---
+++ Warshall/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Warshall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
